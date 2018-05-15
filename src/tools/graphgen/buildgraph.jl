3454c288fff8b697b7157bc9b142011fe4b396fc
Nico Kemnitz <nkemnitz@princeton.edu>
2018-05-15 16:53:53 -0400

    feat(graphgen): snippets for parallel generation

--- src/tools/graphgen/buildgraph.jl
+++ src/tools/graphgen/buildgraph.jl
@@ -1,25 +1,39 @@
-push!(LOAD_PATH, dirname(@__FILE__))
-include("../../chunkedgraphs/ChunkedGraphs.jl")
-using ChunkedGraphs
-using CloudVolume
+addprocs(50)
+@everywhere include("../../chunkedgraphs/ChunkedGraphs.jl")
+@everywhere using ChunkedGraphs
+@everywhere using CloudVolume
+@everywhere using Retry
 
-import PyCall
 import JSON
 
-settings = JSON.parsefile("graph.conf")
+settings = JSON.parsefile("pinky40.conf")
 
-function buildgraphbase!(graph::ChunkedGraph, edgetasks::StorageWrapper, ranges::Tuple{StepRange,StepRange,StepRange})
+@everywhere function buildgraphbase!(settings::Dict{String,Any}, graph::ChunkedGraph, edgetasks::StorageWrapper, ranges::Tuple{StepRange,StepRange,StepRange})
 	total = length(ranges[1]) * length(ranges[2]) * length(ranges[3])
 	i = 0
 	for z in ranges[3], y in ranges[2], x in ranges[1]
 		i += 1
 		chunkid = ChunkedGraphs.world_to_chunkid(graph, (x, y, z))
 		prefix = "$(x)-$(x + step(ranges[1]))_$(y)-$(y + step(ranges[2]))_$(z)-$(z + step(ranges[3]))"
-		atomicedges = edgetasks.val[:get_file]("$(prefix)_atomicedges.bin")
-		atomicedges = Vector{AtomicEdge}(reinterpret(AtomicEdge, Vector{UInt8}(atomicedges)))
+		atomicedges = nothing
+		@repeat 7 try
+			atomicedges = edgetasks.val[:get_file]("$(prefix)_atomicedges.bin")
+			atomicedges = Vector{AtomicEdge}(reinterpret(AtomicEdge, Vector{UInt8}(atomicedges)))
+		catch e
+			println(e)
+			println("Error while processing $(prefix)_atomicedges.bin")
+			@delay_retry if true === true end
+		end
 
-		rg2cg = edgetasks.val[:get_file]("$(prefix)_rg2cg.bin")
-		rg2cg = Dict{UInt64, Label}(reinterpret(Pair{UInt64, Label}, Vector{UInt8}(rg2cg)))
+		rg2cg = nothing
+		@repeat 7 try
+			rg2cg = edgetasks.val[:get_file]("$(prefix)_rg2cg.bin")
+			rg2cg = Dict{UInt64, Label}(reinterpret(Pair{UInt64, Label}, Vector{UInt8}(rg2cg)))
+		catch
+			println(e)
+			println("Error while processing $(prefix)_rg2cg.bin")
+			@delay_retry if true === true end
+		end
 		cg2rg = map(reverse, rg2cg)
 		write("$(settings["graphpath"])/cg2rg_$(ChunkedGraphs.stringify(chunkid)).bin", collect(cg2rg))
 		println("$i/$total | $prefix: Adding $(length(rg2cg)) vertices and $(length(atomicedges)) edges")
@@ -32,35 +46,57 @@ function buildgraphbase!(graph::ChunkedGraph, edgetasks::StorageWrapper, ranges:
 	save!(graph)
 end
 
-function buildgraphlayer!(graph::ChunkedGraph, edgetasks::StorageWrapper, ranges::Tuple{StepRange,StepRange,StepRange},
+@everywhere function buildgraphlayer!(graph::ChunkedGraph, edgetasks::StorageWrapper, ranges::Tuple{StepRange,StepRange,StepRange},
 			interchunk_ranges::Tuple{StepRange,StepRange,StepRange})
 	total = length(interchunk_ranges[1]) * length(ranges[2]) * length(ranges[3]) + 
 			length(ranges[1]) * length(interchunk_ranges[2]) * length(ranges[3]) + 
 			length(ranges[1]) * length(ranges[2]) * length(interchunk_ranges[3])
 
+	println("Building Subgraph using range: $ranges")
 	@time begin
 		i = 0
 		for z in ranges[3], y in ranges[2], x in interchunk_ranges[1]
 			i += 1
 			prefix = "$(x)-$(x + 2)_$(y)-$(y + step(ranges[2]))_$(z)-$(z + step(ranges[3]))"
-			atomicedges = edgetasks.val[:get_file]("$(prefix)_atomicedges.bin")
-			atomicedges = Vector{AtomicEdge}(reinterpret(AtomicEdge, Vector{UInt8}(atomicedges)))
+			atomicedges = nothing
+			@repeat 7 try
+				atomicedges = edgetasks.val[:get_file]("$(prefix)_atomicedges.bin")
+				atomicedges = Vector{AtomicEdge}(reinterpret(AtomicEdge, Vector{UInt8}(atomicedges)))
+			catch e
+				println(e)
+				println("Error while processing $(prefix)_atomicedges.bin")
+				@delay_retry if true === true end
+			end
 			println("$i/$total | $prefix: Adding $(length(atomicedges)) edges")
 			add_atomic_edges!(graph, atomicedges)
 		end
 		for z in ranges[3], y in interchunk_ranges[2], x in ranges[1]
 			i += 1
 			prefix = "$(x)-$(x + step(ranges[1]))_$(y)-$(y + 2)_$(z)-$(z + step(ranges[3]))"
-			atomicedges = edgetasks.val[:get_file]("$(prefix)_atomicedges.bin")
-			atomicedges = Vector{AtomicEdge}(reinterpret(AtomicEdge, Vector{UInt8}(atomicedges)))
+			atomicedges = nothing
+			@repeat 7 try
+				atomicedges = edgetasks.val[:get_file]("$(prefix)_atomicedges.bin")
+				atomicedges = Vector{AtomicEdge}(reinterpret(AtomicEdge, Vector{UInt8}(atomicedges)))
+			catch e
+				println(e)
+				println("Error while processing $(prefix)_atomicedges.bin")
+				@delay_retry if true === true end
+			end
 			println("$i/$total | $prefix: Adding $(length(atomicedges)) edges")
 			add_atomic_edges!(graph, atomicedges)
 		end
 		for z in interchunk_ranges[3], y in ranges[2], x in ranges[1]
 			i += 1
 			prefix = "$(x)-$(x + step(ranges[1]))_$(y)-$(y + step(ranges[2]))_$(z)-$(z + 2)"
-			atomicedges = edgetasks.val[:get_file]("$(prefix)_atomicedges.bin")
-			atomicedges = Vector{AtomicEdge}(reinterpret(AtomicEdge, Vector{UInt8}(atomicedges)))
+			atomicedges = nothing
+			@repeat 7 try
+				atomicedges = edgetasks.val[:get_file]("$(prefix)_atomicedges.bin")
+				atomicedges = Vector{AtomicEdge}(reinterpret(AtomicEdge, Vector{UInt8}(atomicedges)))
+			catch e
+				println(e)
+				println("Error while processing $(prefix)_atomicedges.bin")
+				@delay_retry if true === true end
+			end
 			println("$i/$total | $prefix: Adding $(length(atomicedges)) edges")
 			add_atomic_edges!(graph, atomicedges)
 		end
@@ -70,7 +106,7 @@ function buildgraphlayer!(graph::ChunkedGraph, edgetasks::StorageWrapper, ranges
 	end
 end
 
-function buildgraph(settings::Dict{String,Any})
+@everywhere function buildgraph(settings::Dict{String,Any})
 	# Ensure graph directory exists
 	if !isdir(settings["graphpath"])
 		mkdir(settings["graphpath"])
@@ -83,10 +119,10 @@ function buildgraph(settings::Dict{String,Any})
 	ranges = ([settings["offset"][i] : settings["chunksize"][i] : settings["offset"][i] + settings["size"][i] - 1 for i in 1:3]...)
 
 	# Build Base Layer
-	buildgraphbase!(graph, edgetasks, ranges)
+	buildgraphbase!(settings, graph, edgetasks, ranges)
 
 	newstep = settings["chunksize"]
-	iterations = Int(ceil(log2(maximum(cld.(settings["offset"] .+ settings["size"], settings["chunksize"])))))
+	iterations = settings["maxdepth"] - 2 #Int(ceil(log2(maximum(cld.(settings["offset"] .+ settings["size"], settings["chunksize"])))))
 	for j = 1:iterations
 		newstep = 2 * newstep
 		newstart = @. newstep * cld(settings["offset"], newstep) + div(newstep, 2) - 1
@@ -103,42 +139,55 @@ function prevmult(n::Integer, m::Integer)
 	return -nextmult(-n, m)
 end
 
-roisize = prevpow2.(last.(ranges))
+function buildgraph(settings::Dict{String,Any}, subdivisions::Integer=2)
+	ranges = ([settings["offset"][i] : settings["chunksize"][i] : settings["offset"][i] + settings["size"][i] - 1 for i in 1:3]...)
 
-minpoint = (settings["offset"]...)
-midpoint = prevpow2.(last.(ranges))
-maxpoint = ((settings["offset"] .+ settings["size"])...)
+	global_size = (nextpow2.(settings["size"])...)
+	global_max = ((settings["offset"] .+ settings["size"])...)
+	global_max_pow2 = nextpow2.(global_max)
+	global_min = global_max_pow2 .- global_size
+	block_count = 2^subdivisions
+	block_size = maximum(div.(global_size, settings["chunksize"])) * div.(settings["chunksize"], block_count)
 
+	new_ranges = collect.(range.(global_min, block_size, block_count))
+	new_settings = Vector{Dict{String,Any}}()
 
-function buildgraph_recursive(settings::Dict{String, Any}, bbox::Tuple{StepRange{Int64},StepRange{Int64},StepRange{Int64}})
-	if any(last.(bbox) .< (settings["offset"]...)) ||
-			any(start.(bbox)) .> ((settings["offset"] .+ settings["size"])...)
-		return
+	for x in new_ranges[1]
+		for y in new_ranges[2]
+			for z in new_ranges[3]
+				s = deepcopy(settings)
+				s["offset"] = max.(settings["offset"], [x, y, z])
+				if any(s["offset"] .>= global_max)
+					continue
+				end
+				block_idx = div.(s["offset"], block_size)
+				s["size"] = min.(nextmult.(s["offset"] .+ 1, block_size) .- s["offset"], global_max .- s["offset"])
+				s["graphpath"] = "$(settings["graphpath"])/$(subdivisions)_$(block_idx[1])_$(block_idx[2])_$(block_idx[3])"
+				s["maxdepth"] = settings["maxdepth"] - subdivisions
+				push!(new_settings, s)
+			end
+		end
 	end
 
+	pmap(buildgraph, new_settings)
 
-
-end
-
-# Dataset origin and World origin are usually not the same. We keep it that way
-# as it makes converting chunks to coordinates a bit easier...
-function buildgraph(settings::Dict{String, Any})
-	chunksize = (settings["step"]...)
-	datasetsize = (settings["size"]...)
-	datasetbbox = range.((settings["offset"]...), datasetsize)
-	worldbbox = range.((0, 0, 0), nextpow2.(last.(datasetbbox)))
-	depth = Int(log2(maximum(div.(last.(worldbbox), chunksize))))
-
-	halfsize = 2^(depth-1) .* chunksize
-	offsets = first.(worldbbox)
-
-	buildgraph_recursive(range.(offsets, halfsize, 2))
-
-
-
-	midpoint = div.(last.(ranges) .- first.(ranges), 2)
+	# Move to common folder
 	
-	right = nextpow2.(last.(ranges))
-	mid = prevpow2.(r .- 1)
-	left = r .- 2 .* m
+	# Run remaining edges
+	# Init Cloud Storage and Chunked Graph
+	#graph = ChunkedGraph(settings["graphpath"], settings)
+	#edgetasks = StorageWrapper(settings["edgetasks"])
+
+	#ranges = ([settings["offset"][i] : settings["chunksize"][i] : settings["offset"][i] + settings["size"][i] - 1 for i in 1:3]...)
+
+	#newstep = settings["chunksize"]
+	#iterations = Int(ceil(log2(maximum(cld.(settings["offset"] .+ settings["size"], settings["chunksize"])))))
+	#for j = 1:iterations
+	#	newstep = 2 * newstep
+	#	newstart = @. newstep * cld(settings["offset"], newstep) + div(newstep, 2) - 1
+	#	interchunk_ranges = ([newstart[i] : newstep[i] : settings["offset"][i] + settings["size"][i] for i in 1:3]...)
+	#	buildgraphlayer!(graph, edgetasks, ranges, interchunk_ranges)
+	#end
+
 end
+buildgraph(settings, 3)
